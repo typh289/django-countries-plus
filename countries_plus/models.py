a4ff9542dff9ecea23c3b829f4f8742adb98a11d
Gio <giorgio.corrente@x3system.it>
2021-07-08 09:52:02 +0200

    Ugettext deprecated in django 4

--- countries_plus/models.py
+++ countries_plus/models.py
@@ -3,7 +3,7 @@ import logging
 import six
 from django.core.exceptions import ObjectDoesNotExist
 from django.db import models
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 logger = logging.getLogger(__name__)
 
